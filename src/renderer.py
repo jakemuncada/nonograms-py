910f78a6361d93a77ef8bdbc44da946886dbf0a6
Jake <jek.muncada@gmail.com>
2022-12-03 18:40:28 +0900

    Implemented clue panels.

--- src/renderer.py
+++ src/renderer.py
@@ -4,6 +4,7 @@ Module for rendering the game to the screen.
 
 import pygame
 import logging
+from typing import Optional
 
 from src.coord import Coord
 from src.puzzle import Puzzle
@@ -12,8 +13,11 @@ import src.utils.render_utils as utils
 
 
 BLACK = (0, 0, 0)
+GRAY = (144, 144, 144)
+RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BROWN = (138, 94, 56)
+LIGHT_BROWN = (217, 183, 165)
 
 logger = logging.getLogger(__name__)
 console = logging.getLogger('console')
@@ -24,23 +28,42 @@ class Renderer:
     Renders the game to the screen.
     """
 
+    FLAG_BORDER_NONE = 0
+    FLAG_BORDER_TOP = 1 << 0
+    FLAG_BORDER_RIGHT = 1 << 1
+    FLAG_BORDER_BOT = 1 << 2
+    FLAG_BORDER_LEFT = 1 << 3
+    FLAG_BORDER_ALL = FLAG_BORDER_TOP | FLAG_BORDER_LEFT | FLAG_BORDER_RIGHT | FLAG_BORDER_BOT
+
     def __init__(self, screen: pygame.Surface) -> None:
         self.__screen = screen
         self.__screen.fill(BROWN)
+
+        self.__fonts: list[pygame.font.Font] = []
+        self.__fonts.append(pygame.font.SysFont('consolas', 10))
+        self.__fonts.append(pygame.font.SysFont('consolas', 13))
+        self.__fonts.append(pygame.font.SysFont('consolas', 16))
+        self.__fonts.append(pygame.font.SysFont('consolas', 20))
+        self.__fonts.append(pygame.font.SysFont('consolas', 24))
+        self.__fonts.append(pygame.font.SysFont('consolas', 28))
+        self.__fonts.append(pygame.font.SysFont('consolas', 32))
+        self.__fonts.append(pygame.font.SysFont('consolas', 36))
 
         self.puzzle: Puzzle = None
         self.total_rows: int = 0
         self.total_cols: int = 0
 
-        self.cell_width: float = 0.0
-        self.cell_height: float = 0.0
+        self.cell_size: float = 0.0
 
         self.__scale_factor: float = 1.0
         self.__pan_delta: Coord = Coord((0, 0))
 
+        self.bdr_thick = constants.BORDER_THICKNESS
+
         self.board_rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
         self.top_clues_rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
         self.left_clues_rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
+        self.parent_rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
 
         self.board_surface: pygame.Surface = None
         """The board surface that contains the symbols."""
@@ -66,6 +89,10 @@ class Renderer:
         """The pan delta."""
         return self.__scale_factor
 
+    ################################################################################################
+    # INITIALIZATION METHODS
+    ################################################################################################
+
     def initialize_puzzle(self, puzzle: Puzzle) -> None:
         """
         Initialize the puzzle to be displayed.
@@ -80,9 +107,9 @@ class Renderer:
         """
         Initialize all the puzzle surfaces.
         """
-        self.__initialize_board_surface()
+        self.__initialize_surfaces()
 
-    def __initialize_board_surface(self) -> None:
+    def __initialize_surfaces(self) -> None:
         """
         Initialize the board surface.
         """
@@ -90,86 +117,54 @@ class Renderer:
         ncols = self.puzzle.ncols
         top_clues_nrows = self.puzzle.max_col_clues
         left_clues_ncols = self.puzzle.max_row_clues
+        bdr_thick = constants.BORDER_THICKNESS
+        bdr_thick2 = bdr_thick * 2
 
         # Calculate the optimum cell size where the puzzle fills the screen.
         optimum_cell_size = utils.calc_optimum_cell_size(nrows, ncols, top_clues_nrows, left_clues_ncols)
         # Then, apply the scaling factor, i.e. magnification.
         self.cell_size = optimum_cell_size * self.__scale_factor
 
+        # Get the sizes and positions of the board, and the clues panels.
         board_rect, top_clues_rect, left_clues_rect, parent_rect = utils.calc_rects(
             self.cell_size, self.cell_size, self.cell_size,
             nrows, ncols, top_clues_nrows, left_clues_ncols)
 
+        # Save the calculated values to their respective properties.
         self.board_rect = board_rect
         self.top_clues_rect = top_clues_rect
         self.left_clues_rect = left_clues_rect
         self.parent_rect = parent_rect
 
+        # Create the board surface.
         self.board_surface = pygame.Surface((board_rect.width, board_rect.height))
         self.board_surface.fill(WHITE)
 
+        # Create the top clues panel surface.
         self.top_clues_surface = pygame.Surface((top_clues_rect.width, top_clues_rect.height))
-        self.top_clues_surface.fill(WHITE)
+        self.top_clues_surface.fill(LIGHT_BROWN)
 
+        # Create the left clues panel surface.
         self.left_clues_surface = pygame.Surface((left_clues_rect.width, left_clues_rect.height))
-        self.left_clues_surface.fill(WHITE)
+        self.left_clues_surface.fill(LIGHT_BROWN)
 
+        # Draw the outer borders.
+        self.__draw_rect_borders(self.board_surface, bdr_thick * 2)
+        self.__draw_rect_borders(self.top_clues_surface, bdr_thick * 2)
+        self.__draw_rect_borders(self.left_clues_surface, bdr_thick * 2)
 
-        _, _, width, height = self.board_rect
-        bdr = constants.BORDER_THICKNESS
+        # Draw the cell borders.
+        self.__draw_cell_borders(self.board_surface, nrows, ncols, self.cell_size, bdr_thick, None, bdr_thick2)
+        self.__draw_cell_borders(self.top_clues_surface, top_clues_nrows, ncols, self.cell_size, bdr_thick, None, bdr_thick2)
+        self.__draw_cell_borders(self.left_clues_surface, nrows, left_clues_ncols, self.cell_size, bdr_thick, None, bdr_thick2)
 
-        # Draw board outer borders.
-        for i in range(constants.BORDER_THICKNESS * 2):
-            top_1 = (0, i)
-            top_2 = (width + bdr, i)
-            right_1 = (width - (i + 1), 0)
-            right_2 = (width - (i + 1), height + bdr)
-            bot_1 = (0, height - (i + 1))
-            bot_2 = (width + bdr, height - (i + 1))
-            left_1 = (i, 0)
-            left_2 = (i, height + bdr)
-            pygame.draw.line(self.board_surface, BLACK, top_1, top_2, 1)
-            pygame.draw.line(self.board_surface, BLACK, right_1, right_2, 1)
-            pygame.draw.line(self.board_surface, BLACK, bot_1, bot_2, 1)
-            pygame.draw.line(self.board_surface, BLACK, left_1, left_2, 1)
+        # Draw the clue numbers.
+        self.__draw_top_clues_numbers()
+        self.__draw_left_clues_numbers()
 
-        # Draw horizontal cell borders.
-        for i in range(constants.BORDER_THICKNESS):
-            for row_idx in range(self.puzzle.nrows - 1):
-                y = bdr * 2                             # Skip the top border.
-                y += (self.cell_size + bdr) * row_idx   # Skip the previous rows.
-                y += i                                  # Skip the previously drawn borders.
-                y += self.cell_size                     # Draw the line below the current row.
-                p1 = (0, y)
-                p2 = (width + bdr, y)
-                pygame.draw.line(self.board_surface, BLACK, p1, p2, 1)
-
-        # Draw vertical cell borders.
-        for i in range(constants.BORDER_THICKNESS):
-            for col_idx in range(self.puzzle.ncols - 1):
-                x = bdr * 2                             # Skip the left border.
-                x += (self.cell_size + bdr) * col_idx   # Skip the previous columns.
-                x += i                                  # Skip the previously drawn borders.
-                x += self.cell_size                     # Draw the line to the right of the current column.
-                p1 = (x, 0)
-                p2 = (x, height + bdr)
-                pygame.draw.line(self.board_surface, BLACK, p1, p2, 1)
-
-    def __initialize_top_clues_surface(self) -> None:
-        """
-        Initialize the top clues panel surface.
-
-        Note that the board surface must already be initialized.
-        """
-        width = self.board_rect.width
-        height = utils.calc_clues_panel_dimension(self.cell_size, self.puzzle.max_col_clues)
-        left = self.board_rect.left
-        top = self.board_rect.top - height
-
-        self.top_clues_rect = pygame.Rect(left, top, width, height)
-
-        self.top_clues_surface = pygame.Surface((width, height))
-        self.top_clues_surface.fill(WHITE)
+    ################################################################################################
+    # USER INTERACTION METHODS
+    ################################################################################################
 
     def set_pan(self, new_pan_x: float, new_pan_y: float) -> None:
         """
@@ -198,15 +193,196 @@ class Renderer:
         """
         self.set_scaling_factor(self.scale_factor + value)
 
+    ################################################################################################
+    # RENDER METHOD
+    ################################################################################################
+
     def render(self) -> None:
         """
         Render the current puzzle.
         """
-        board_x = self.board_rect.x + self.__pan_delta.x
-        board_y = self.board_rect.y + self.__pan_delta.y
-        self.screen.blit(self.board_surface, (board_x, board_y))
+        render_list: list[tuple[pygame.Surface, pygame.Rect]] = []
 
-        top_clues_y = self.top_clues_rect.y + self.__pan_delta.y
-        self.screen.blit(self.top_clues_surface, (board_x, top_clues_y))
+        board_x = self.parent_rect.x + self.board_rect.x + self.__pan_delta.x
+        board_y = self.parent_rect.y + self.board_rect.y + self.__pan_delta.y
+        board_rect = self.board_surface.get_rect().move(board_x, board_y)
+        render_list.append((self.board_surface, board_rect))
 
+        top_clues_y = self.parent_rect.y + self.top_clues_rect.y + self.__pan_delta.y
+        top_clues_rect = self.top_clues_surface.get_rect().move(board_x, top_clues_y)
+        render_list.append((self.top_clues_surface, top_clues_rect))
+
+        left_clues_x = self.parent_rect.x + self.left_clues_rect.x + self.__pan_delta.x
+        left_clues_rect = self.left_clues_surface.get_rect().move(left_clues_x, board_y)
+        render_list.append((self.left_clues_surface, left_clues_rect))
+
+        self.screen.blits(render_list)
         pygame.display.update()
+
+    ################################################################################################
+    # DRAW HELPER METHODS
+    ################################################################################################
+
+    def __draw_rect_borders(self, surface: pygame.Surface, thickness: int,
+        rect: Optional[pygame.Rect] = None, flags: int = FLAG_BORDER_ALL, color: tuple = BLACK) -> None:
+        """
+        Draw the borders with size and position of the given rect.
+
+        Parameters:
+            surface: The surface to draw on.
+            thickness: The border thickness.
+            rect: The rect that dictates the size and position of the border/s to draw.
+                  If not specified, the border will be drawn on the outer edges of the given surface.
+            flags: The flag that dictates which border side/s to draw.
+                   If not specified, borders on all sides will be drawn.
+            color: The border color.
+        """
+        if rect is None:
+            rect = pygame.Rect(0, 0, surface.get_width(), surface.get_height())
+
+        for i in range(thickness):
+            if flags & Renderer.FLAG_BORDER_TOP:
+                p1 = (0, i)
+                p2 =  (rect.width - 0.5, i)
+                pygame.draw.line(surface, color, p1, p2, 1)
+            
+            if flags & Renderer.FLAG_BORDER_RIGHT:
+                p1 = (rect.width - (i + 1), 0)
+                p2 = (rect.width - (i + 1), rect.height - 0.5)
+                pygame.draw.line(surface, color, p1, p2, 1)
+                
+            if flags & Renderer.FLAG_BORDER_BOT:
+                p1 = (0, rect.height - (i + 1))
+                p2 = (rect.width - 0.5, rect.height - (i + 1))
+                pygame.draw.line(surface, color, p1, p2, 1)
+
+            if flags & Renderer.FLAG_BORDER_LEFT:
+                p1 = (i, 0)
+                p2 = (i, rect.height - 0.5)
+                pygame.draw.line(surface, color, p1, p2, 1)
+
+    def __draw_cell_borders(self,
+        surface: pygame.Surface,
+        nrows: int,
+        ncols: int,
+        cell_size: float,
+        thickness: int,
+        rect: Optional[pygame.Rect] = None,
+        offset_outer_bdr: Optional[float] = 0,
+        color: tuple = BLACK) -> None:
+        """
+        Draw the cell borders.
+
+        Parameters:
+            surface: The surface to draw on.
+            nrows: The number of rows.
+            ncols: The number of columns.
+            cell_size: The cell size.
+            thickness: The border thickness.
+            rect: The rect where the cells are found, not including outer border.
+                  If not specified, the whole surface will be treated as the rect.
+            offset_outer_bdr: How many pixels to offset from the outer border.
+            color: The line color.
+        """
+        if rect is None:
+            rect = pygame.Rect(0, 0, surface.get_width(), surface.get_height())
+
+        if offset_outer_bdr > 0:
+            rect = pygame.Rect(
+                rect.x + offset_outer_bdr,
+                rect.y + offset_outer_bdr,
+                rect.width - offset_outer_bdr * 2,
+                rect.height - offset_outer_bdr * 2)
+
+        # Draw horizontal cell borders.
+        for i in range(thickness):
+            for row_idx in range(nrows - 1):
+                y = rect.y
+                y += (cell_size + thickness) * row_idx   # Skip the previous rows.
+                y += i                             # Skip the previously drawn borders.
+                y += cell_size                     # Draw the line below the current row.
+                p1 = (rect.x, y)
+                p2 = (rect.x + rect.width + 0.5, y)
+                pygame.draw.line(surface, color, p1, p2, 1)
+
+        # Draw vertical cell borders.
+        for i in range(thickness):
+            for col_idx in range(ncols - 1):
+                x = rect.x
+                x += (cell_size + thickness) * col_idx  # Skip the previous columns.
+                x += i                                  # Skip the previously drawn borders.
+                x += cell_size                     # Draw the line to the right of the current column.
+                p1 = (x, rect.y)
+                p2 = (x, rect.y + rect.height + 0.5)
+                pygame.draw.line(surface, color, p1, p2, 1)
+
+    def __draw_top_clues_numbers(self) -> None:
+        """
+        Draw the top clues cell numbers.
+        """
+        render_list: list[tuple[pygame.Surface, pygame.Rect]] = []
+        font = self.__get_font(self.cell_size, self.cell_size)
+
+        for col_idx in range(len(self.puzzle.col_clues)):
+            clues = self.puzzle.col_clues[col_idx]
+            for row_idx in range(len(clues)):
+                r_idx = self.puzzle.max_col_clues - row_idx - 1
+                num = clues[row_idx]
+                cell_rect = utils.get_cell_rect(r_idx, col_idx, self.cell_size, self.cell_size, self.bdr_thick)
+                cell_rect = cell_rect.move(self.bdr_thick * 2, self.bdr_thick * 2)
+                # pygame.draw.rect(self.top_clues_surface, RED, cell_rect, 1)
+                text_surface = font.render(str(num), True, BLACK)
+                text_rect = text_surface.get_rect()
+                x = cell_rect.centerx - text_rect.centerx
+                y = cell_rect.centery - text_rect.centery + (text_rect.height / 12)
+                draw_rect = pygame.Rect(x, y, text_rect.width, text_rect.height)
+                # pygame.draw.rect(self.top_clues_surface, GRAY, draw_rect)
+                render_list.append((text_surface, draw_rect))
+
+        self.top_clues_surface.blits(render_list)
+
+    def __draw_left_clues_numbers(self) -> None:
+        """
+        Draw the left clues cell numbers.
+        """
+        render_list: list[tuple[pygame.Surface, pygame.Rect]] = []
+        font = self.__get_font(self.cell_size, self.cell_size)
+
+        for row_idx in range(len(self.puzzle.row_clues)):
+            clues = self.puzzle.row_clues[row_idx]
+            for col_idx in range(len(clues)):
+                c_idx = self.puzzle.max_row_clues - col_idx - 1
+                num = clues[col_idx]
+                cell_rect = utils.get_cell_rect(row_idx, c_idx, self.cell_size, self.cell_size, self.bdr_thick)
+                cell_rect = cell_rect.move(self.bdr_thick * 2, self.bdr_thick * 2)
+                # pygame.draw.rect(self.top_clues_surface, RED, cell_rect, 1)
+                text_surface = font.render(str(num), True, BLACK)
+                text_rect = text_surface.get_rect()
+                x = cell_rect.centerx - text_rect.centerx
+                y = cell_rect.centery - text_rect.centery + (text_rect.height / 12)
+                draw_rect = pygame.Rect(x, y, text_rect.width, text_rect.height)
+                # pygame.draw.rect(self.top_clues_surface, GRAY, draw_rect)
+                render_list.append((text_surface, draw_rect))
+
+        self.left_clues_surface.blits(render_list)
+
+    def __get_font(self, cell_width: float, cell_height: float) -> pygame.font.Font:
+        """
+        Get the optimal font for the cell with the given size.
+        """
+        mini = min(cell_width, cell_height)
+        if mini >= 45:
+            return self.__fonts[-1]
+        if mini >= 42:
+            return self.__fonts[-2]
+        if mini >= 38:
+            return self.__fonts[-3]
+        if mini >= 34:
+            return self.__fonts[-4]
+        if mini >= 30:
+            return self.__fonts[-5]
+        if mini >= 27:
+            return self.__fonts[-6]
+        if mini >= 22:
+            return self.__fonts[-7]
+        return self.__fonts[-8]
--- src/utils/render_utils.py
+++ src/utils/render_utils.py
@@ -5,6 +5,27 @@ Some useful utility functions for the renderer.
 import pygame
 import src.constants as constants
 
+def offset_rect(rect: pygame.Rect, amount: float) -> pygame.Rect:
+    """
+    Offset a rect by the specified amount and return the resulting rect.
+
+    Offsetting will increase/decrease the boundaries of the rect by the specified amount.
+    
+    A positive value will decrease the boundaries of the rect while a negative value
+    will increase the boundaries of the rect.
+
+    Parameters:
+        rect: The rect to be offset.
+        amount: The offset value.
+
+    Returns:
+        A new Rect instance whose boundaries are offset from the original rect.
+    """
+    x = rect.x + amount
+    y = rect.y + amount
+    width = rect.width - (amount * 2)
+    height = rect.height - (amount * 2)
+    return pygame.Rect(x, y, width, height)
 
 
 def calc_rects(
@@ -38,10 +59,13 @@ def calc_rects(
         - The size and position of the left clues panel.
         - The size and position of the parent rect that contains the above.
     """
-    board_border_thick_h = ((board_ncols - 1 + 4) * constants.BORDER_THICKNESS)
-    board_border_thick_v = ((board_nrows - 1 + 4) * constants.BORDER_THICKNESS)
-    top_clues_border_thick_v = ((top_clues_nrows - 1 + 2) * constants.BORDER_THICKNESS)
-    left_clues_border_thick_h = ((left_clues_ncols - 1 + 2) * constants.BORDER_THICKNESS)
+    bdr_thick = constants.BORDER_THICKNESS
+    bdr_thick2 = bdr_thick * 2
+
+    board_border_thick_h = ((board_ncols - 1 + 4) * bdr_thick)
+    board_border_thick_v = ((board_nrows - 1 + 4) * bdr_thick)
+    top_clues_border_thick_v = ((top_clues_nrows - 1 + 4) * bdr_thick)
+    left_clues_border_thick_h = ((left_clues_ncols - 1 + 4) * bdr_thick)
 
     total_border_thick_h = board_border_thick_h + left_clues_border_thick_h
     total_border_thick_v = board_border_thick_v + top_clues_border_thick_v
@@ -54,8 +78,8 @@ def calc_rects(
     total_cell_size_h = board_cell_size_h + left_clues_cell_size_h
     total_cell_size_v = board_cell_size_v + top_clues_cell_size_v
 
-    parent_w = total_cell_size_h + total_border_thick_h
-    parent_h = total_cell_size_v + total_border_thick_v
+    parent_w = total_cell_size_h + total_border_thick_h - bdr_thick2
+    parent_h = total_cell_size_v + total_border_thick_v - bdr_thick2
     parent_x = constants.SCREEN_HALF_WIDTH - (parent_w / 2)
     parent_y = constants.SCREEN_HALF_HEIGHT - (parent_h / 2)
     parent_rect = pygame.Rect(parent_x, parent_y, parent_w, parent_h)
@@ -64,19 +88,19 @@ def calc_rects(
     board_h = board_border_thick_v + board_cell_size_v
     board_x = parent_w - board_w
     board_y = parent_h - board_h
-    board_rect = pygame.Rect(board_w, board_h, board_x, board_y)
+    board_rect = pygame.Rect(board_x, board_y, board_w, board_h)
 
     top_clues_w = board_w
     top_clues_h = top_clues_border_thick_v + top_clues_cell_size_v
     top_clues_x = board_x
     top_clues_y = 0
-    top_clues_rect = pygame.Rect(top_clues_w, top_clues_h, top_clues_x, top_clues_y)
+    top_clues_rect = pygame.Rect(top_clues_x, top_clues_y, top_clues_w, top_clues_h)
 
     left_clues_w = left_clues_border_thick_h + left_clues_cell_size_h
     left_clues_h = board_h
     left_clues_x = 0
     left_clues_y = board_y
-    left_clues_rect = pygame.Rect(left_clues_w, left_clues_h, left_clues_x, left_clues_y)
+    left_clues_rect = pygame.Rect(left_clues_x, left_clues_y, left_clues_w, left_clues_h)
 
     return board_rect, top_clues_rect, left_clues_rect, parent_rect
 
@@ -120,94 +144,30 @@ def calc_optimum_cell_size(
     return min(cell_width, cell_height)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def calc_borders_thickness(nrows: int, ncols: int) -> tuple[float, float]:
+def get_cell_rect(
+    row: int,
+    col: int,
+    cell_width: float,
+    cell_height: float,
+    bdr: int) -> pygame.Rect:
     """
-    Calculate the total border thickness for both horizontal and vertical borders.
+    Get the rect of the specified cell.
 
-    The total border thickness includes the following:
-        - Two border width's worth of outer border on the one side.
-        - All the inner borders (between the cells).
-        - And another two border width's worth of outer border on the other side.
+    This rect is relative to the rect whose (0, 0) coordinate
+    is at the upper left grid corner.
 
     Parameters:
-        nrows: The number of rows.
-        ncols: The number of columns.
+        row: The specified cell's row index.
+        col: The specified cell's column index.
+        cell_width: The cell width.
+        cell_height: The cell height.
+        bdr: The border thickness.
 
     Returns:
-        A tuple containing the horizontal borders thickness
-        and the vertical borders thickness respectively.
+        The rect of the specified cell.
     """
-    hborders_count = (ncols - 1) + 4
-    vborders_count = (nrows - 1) + 4
-    hborders_thickness = hborders_count * constants.BORDER_THICKNESS
-    vborders_thickness = vborders_count * constants.BORDER_THICKNESS
-    return hborders_thickness, vborders_thickness
-
-
-
-def calc_board_rect(cell_size: float, nrows: int, ncols: int) -> pygame.Rect:
-    """
-    Calculate the board rect.
-
-    Parameters:
-        cell_size: The size of the cells in pixels.
-        nrows: The number of rows in the puzzle.
-        ncols: The number of columns in the puzzle.
-
-    Returns:
-        The board rect, which specifies the position and size of the board.
-    """
-    borders_h, borders_v = calc_borders_thickness(nrows, ncols)
-    board_width = (cell_size * ncols) + borders_h
-    board_height = (cell_size * nrows) + borders_v
-    board_x = constants.SCREEN_HALF_WIDTH - (board_width / 2)
-    board_y = constants.SCREEN_HALF_HEIGHT - (board_width / 2)
-    return pygame.Rect(board_x, board_y, board_width, board_height)
-
-def calc_top_clues_rect(cell_size: float, nrows: int, ncols: int) -> pygame.Rect:
-    """
-    """
-    # borders_h, borders_v = calc_borders_thickness(nrows, ncols)
-    # width = (cell_size * ncols) + borders_h
-    # height = (cell_size * nrows) + borders_v - (constants.BORDER_THICKNESS * 2)
-    # board_x = constants.SCREEN_HALF_WIDTH - (board_width / 2)
-
-    # width = (cell_size * ncols) + hborders_thickness
-    # height = utils.calc_clues_panel_dimension(self.cell_size, self.puzzle.max_col_clues)
-    # left = self.board_rect.left
-    # top = self.board_rect.top - height
-
-def calc_clues_panel_dimension(cell_size: float, max_clues: int) -> float:
-    """
-    Calculate either the width/height dimension of the left/top clues panel,
-    depending on the given parameters.
-
-    Parameters:
-        cell_size: The size of the cells in pixels.
-        max_clues: The maximum number of clues.
-
-    Returns:
-        Either the width of the left clues panel or the height of the top clues panel.
-    """
-    nborders = (max_clues - 1) + 2
-    borders_thickness = nborders * constants.BORDER_THICKNESS
-    return (cell_size * max_clues) + borders_thickness
+    x = (col * cell_width) + (col * bdr)
+    y = (row * cell_height) + (row * bdr)
+    width = cell_width
+    height = cell_height
+    return pygame.Rect(x, y, width, height)
