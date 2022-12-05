06c5976de0c6f60082d294c7c5d8eea5b3891643
jake <Muncada.Jakerandolphbello.lu@mesw.co.jp>
2022-12-05 13:22:16 +0900

    Improved drawing of the X symbol.

--- src/renderer.py
+++ src/renderer.py
@@ -402,13 +402,32 @@ class Renderer:
         if symbol == 'x':
             pygame.draw.rect(self.symbol_surface, colors.PUZZLE_BG, symbol_rect)
             line_w = 2
+
             p1, p2 = symbol_rect.topleft, symbol_rect.bottomright
             p2 = p2[0] - line_w / 2, p2[1]
-            pygame.draw.aaline(self.symbol_surface, color, p1, p2)
+
+            p1b = (p1[0] + 1, p1[1])
+            p1c = (p1[0], p1[1] + 1)
+
+            p2b = (p2[0], p2[1] - 1)
+            p2c = (p2[0] - 1, p2[1])
+
+            pygame.draw.aaline(self.symbol_surface, color, p1, p2, 0)
+            pygame.draw.aaline(self.symbol_surface, color, p1b, p2b, 0)
+            pygame.draw.aaline(self.symbol_surface, color, p1c, p2c, 0)
 
             p1, p2 = symbol_rect.bottomleft, symbol_rect.topright
             p2 = p2[0] - line_w / 2, p2[1]
-            pygame.draw.aaline(self.symbol_surface, color, p1, p2)
+
+            p1b = p1[0] + 1,    p1[1]
+            p1c = p1[0],        p1[1] - 1
+
+            p2b = p2[0],        p2[1] + 1
+            p2c = p2[0] - 1,    p2[1]
+
+            pygame.draw.aaline(self.symbol_surface, color, p1, p2, 0)
+            pygame.draw.aaline(self.symbol_surface, color, p1b, p2b, 0)
+            pygame.draw.aaline(self.symbol_surface, color, p1c, p2c, 0)
             
             return symbol_rect
         
